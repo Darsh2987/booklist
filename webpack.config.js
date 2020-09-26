d563b25657abd90ae1ba36a746bcc325389a5a46
Darsh <darsh2987@gmail.com>
2020-09-26 22:43:34 +0100

    restructured folder for github workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./app.js",
+  entry: "./src/app.js",
   output: {
     filename: "bundled-script.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "index",
+      template: "src/index.html",
+    }),
+  ],
 };
